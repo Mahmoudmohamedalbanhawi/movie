a8954d5772185cb8f69592cc5edde4fe63138d47
mahmoudalbanhawi <mahmoudalbanhawi863@gmail.com>
2025-08-27 11:38:00 +0300

    set loading and error

--- src/App.js
+++ src/App.js
@@ -9,16 +9,29 @@ function App() {
   const apiKey = "ad8ee689";
   const [query , setQuery] = useState("avengers");
   const [isOpen , setIsOpen] = useState(false);
+  const [isLoading , setIsLoading] = useState(false);
+  const [error , setError] = useState(null);
   function toggle()
   {
     setIsOpen(isOpen => !isOpen);
   }
   async function getMovies()
   {
-    const res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`);
+    try {
+      setIsLoading(true);
+      setError(null);
+      const res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`);
     const data = await res.json();
     setMovies(data.Search)
     console.log(data.Search);
+    setIsLoading(false);
+      
+    } catch (error) {
+      setIsLoading(false);
+      console.log("error" , error)
+      setError("Something went wrong please try again later" );
+    }
+    
 
   }
   useEffect(()=>{
@@ -32,7 +45,7 @@ function App() {
 
     
     <div className='d-flex margin-top-2 justify-content-center main gap-2' >
-       <MoviesList toggle={toggle} movies={movies} isOpen={isOpen}/>
+       <MoviesList isLoading={isLoading} error={error} toggle={toggle} movies={movies} isOpen={isOpen}/>
      
       <WatchedMovies/>
     </div>
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -3,15 +3,19 @@ import './index.css'
 import Movie from "./Movie";
 import './MovieList.css'
 import Toggle from "./toggle";
-export default function MoviesList({toggle , isOpen , movies})
+export default function MoviesList({toggle , error, isOpen , movies , isLoading})
 {
+    console.log("loading" , isLoading)
+    
     console.log("koko" , movies)
     return(
         <React.Fragment>
             <div className="box ">
+                {isLoading && <div className="loading">Loading...</div>}
+                {error && <div className="error">{error}</div>}
                 <Toggle toggle = {toggle} isOpen={isOpen}/>
                 {!isOpen && (<ul className="list container">
-                <Movie movies = {movies}/>
+                {(!isLoading && !error) && <Movie movies = {movies}/>}
             </ul> )}
                
             </div>
